ab370265ea4b2b18783483d2d0589662c0d621ac
andypinet <1585638808@qq.com>
2018-10-28 10:15:56 +0800

    修复watch模式下bug:dollar:

--- plugin.js
+++ plugin.js
@@ -5,6 +5,11 @@ let jsFiles = []
 class MyPlugin {
   apply(compiler) {
     const self = this
+    compiler.hooks.watchRun.tap('MyPlugin', compiler => {
+      if (jsFiles.length > 1) {
+        jsFiles = []
+      }
+    })
     compiler.hooks.compilation.tap('MyPlugin', compilation => {
       // Staic Plugin interface |compilation |HOOK NAME | register listener
       HtmlWebpackPlugin.getHooks &&
@@ -38,18 +43,16 @@ class MyPlugin {
               })
               data.html = data.html.replace(
                 '__injected__',
-                `
-                +function () {
-                  "use strict"
-                  try {
-                    eval('new class {}');
-                    ${createStr(jsFiles[0])}
-                  } catch(e) {
-                    console.error(e);
-                    ${createStr(jsFiles[1])}
-                  }
-                }()
-                `
+                `+function () {
+      "use strict"
+      try {
+        eval('new class {}');
+        ${createStr(jsFiles[0])}
+      } catch(e) {
+        console.error(e);
+        ${createStr(jsFiles[1])}
+      }
+    }()`
               )
             }
             // Tell webpack to move on
